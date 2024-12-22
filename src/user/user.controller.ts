9567bae416bf8370570e808b5eb3a65201e278b1
Omogo Peter <omogopeter48@gmail.com>
2024-12-22 14:58:27 +0100

    Add new files

---
+++ src/user/user.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+   import { UserService } from './user.service';
+   import { User } from './user.entity';
+
+   @Controller('users')
+   export class UserController {
+     constructor(private readonly userService: UserService) {}
+
+     @Get()
+     findAll(): User[] {
+       return this.userService.findAll();
+     }
+
+     @Get(':id')
+     findOne(@Param('id') id: string): User {
+       return this.userService.findOne(+id);
+     }
+
+     @Post()
+     create(@Body() user: User): User {
+       return this.userService.create(user);
+     }
+
+     @Put(':id')
+     update(@Param('id') id: string, @Body() updatedUser: Partial<User>): User {
+       return this.userService.update(+id, updatedUser);
+     }
+
+     @Delete(':id')
+     delete(@Param('id') id: string): void {
+       this.userService.delete(+id);
+     }
+   }
